0082639002d8279c5ba72fea62f0e3ab178ac2ff
AntonFartovii <72494592+AntonFartovii@users.noreply.github.com>
2022-04-17 12:14:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import {Excel} from './components/excel/Excel';
+import {Toolbar} from "./components/toolbar/Toolbar";
+import {Header} from "./components/header/Header";
+import {Formula} from "./components/formula/Formula";
+import {Table} from "./components/table/Table";
+import './scss/index.scss';
 
-
-import './scss/index.scss'
-import './module'
-
-console.log('Hello world!!!!!!!')
+const excel = new Excel ('#app', {
+    components: [Header, Toolbar, Formula, Table]
+})
+excel.render()
